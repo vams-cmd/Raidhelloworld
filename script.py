61101df1912c1fadc025503f9834e77ef17bd3cf
psaicharan000 <98706227+psaicharan000@users.noreply.github.com>
2022-12-07 10:54:59 +0530

    updated py with bugs

--- script.py
+++ script.py
@@ -4,3 +4,12 @@ def has_duplicates(lst):
     
 x = [1,2,3,4,5,5]
 has_duplicates(x) 			# True
+first = [1, 2, 3, 4, 5]
+print(first)
+second = first
+print(second)
+second.append(6)
+print(second)
+print(first)
+if first == second :
+    print("bug! you will get hacked!")
