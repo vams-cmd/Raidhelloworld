403dddbc8e1aeaaca8859938447b4ce57c658f9d
psaicharan000 <98706227+psaicharan000@users.noreply.github.com>
2022-12-09 17:56:43 +0530

    updated bugcode

--- script.py
+++ script.py
@@ -1,15 +1,37 @@
-print('Hello, World!')
-def has_duplicates(lst):
-    return len(lst) != len(set(lst))
-    
-x = [1,2,3,4,5,5]
-has_duplicates(x) 			# True
-first = [1, 2, 3, 4, 5]
-print(first)
-second = first
-print(second)
-second.append(6)
-print(second)
-print(first)
-if first == second :
-    print("bug! you will get hacked!")
+import string
+
+metaslash = 1
+
+
+def printNames():
+    neal = 'neal'
+    michelle = 'michele'
+    eric = 5
+    print("Local values: %(neal)S %(michele)s %(eric)" % locals())
+
+
+class Nothing:
+    def printValue(value):
+        print(value)
+
+    def set(self, value):
+        self.value = value
+
+
+def tryToDoSomething(self, value):
+    try:
+        import string
+        if not value:
+            raise (RuntimeError, "Hey, there's no value")
+        printNames('a, b, c')
+    except:
+        traceback.print_exc()
+
+
+def setGlobal(value=None):
+    metaslash = value
+    print('Old MetaSlash value is:', metaslash)
+    useless = Nothing(5)
+    print('a useless value is:', useless.valeu)
+
+setGlobal(50)
